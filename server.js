a1f3c24ae922a7f8e481815539c03e762ed976d2
Conor C <Conorc1000@gmail.com>
2015-09-30 16:24:58 +0100

    setup server.js and handler.js, also create status code 200 test

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 
 var http = require('http');
 var port = process.env.PORT || 3000;
-var ac = require('./index.js');
+var handler = require('handler.js');
 var fs = require('fs');
-var index = fs.readFileSync(__dirname + '/index.html');
+//var index = fs.readFileSync(__dirname + '/index.html');
 
-http.createServer(function handler(request, response) {
-  var url = request.url;
-  console.log("request.url:", url);
-  if (url.length === 1) {
-    console.log(index.toString());
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end(index.toString());
+var app = http.createServer(handler);
 
-  }
-  response.end('hello world!');
-
-}).listen(port);
+app.listen(port);
 
 console.log('node http server listening on http://localhost:' + port);
