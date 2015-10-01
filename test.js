9f9e4a05075e33dcb16dce1889a17e226e358f89
thegsi <thegsi@hotmail.com>
2015-10-01 20:11:08 +0100

    first front end test files

---
+++ test.js
@@ -0,0 +1,13 @@
+
+
+  // test('Has the Search button been clicked. You have to physically click the search button within the first 2 seconds to make this test pass', function(assert) {
+  //   var done = assert.async();
+  //   setTimeout (function(){
+  //     var submitButton = document.getElementById('search');
+  //     submitButton.addEventListener("click", function(){
+  //       clicked = true;
+  //     });
+  //     assert.equal( clicked, true, 'Search Button has been clicked' );
+  //     done();
+  //   },2000);
+  // });
